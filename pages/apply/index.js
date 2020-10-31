c994dab2108833c87745eb8b0d980527079908e9
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-31 18:04:42 -0400

    Updating design of apply and thanks pages

--- pages/apply/index.js
+++ pages/apply/index.js
@@ -1,18 +1,35 @@
 import Head from 'next/head';
 
 const ApplyPage = () => (
-  <>
+  <div
+    style={{
+      height: '100%',
+      padding: 32,
+      paddingTop: 80,
+      backgroundImage: "url('/nco-background-purple.png')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      overflowY: 'scroll',
+    }}
+  >
     <Head>
       <title>No-Code Overlays | Apply</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
-    <main style={{ textAlign: 'center', maxWidth: 500, margin: '0 auto' }}>
+    <main
+      style={{
+        textAlign: 'center',
+        maxWidth: 600,
+        margin: '0 auto',
+        backgroundColor: '#000000dd',
+        padding: '48px 32px',
+        color: 'white',
+        borderRadius: '4px',
+      }}
+    >
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginBottom: 16,
+          marginBottom: 32,
         }}
       >
         <img
@@ -20,7 +37,7 @@ const ApplyPage = () => (
           src="/nco-logo-round.png"
           width="100"
           height="100"
-          style={{ marginRight: 12 }}
+          style={{ width: 100, marginBottom: 12 }}
         />
         <h1>Apply to test No-Code Overlays</h1>
       </div>
@@ -53,10 +70,15 @@ const ApplyPage = () => (
         <label htmlFor="link">Link to your channel</label>
         <input id="link" name="link" type="text" />
 
-        <button type="submit">Apply</button>
+        <button
+          type="submit"
+          style={{ backgroundColor: '#dd227c', fontWeight: 'bold' }}
+        >
+          Apply
+        </button>
       </form>
     </main>
-  </>
+  </div>
 );
 
 export default ApplyPage;
--- pages/apply/thanks.js
+++ pages/apply/thanks.js
@@ -1,11 +1,30 @@
+import Head from 'next/head';
+
 const ThanksPage = () => (
-  <div style={{ maxWidth: 500, margin: '0 auto', textAlign: 'center' }}>
+  <div
+    style={{
+      height: '100%',
+      padding: 32,
+      paddingTop: 80,
+      backgroundImage: "url('/nco-background-purple.png')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      overflowY: 'scroll',
+    }}
+  >
+    <Head>
+      <title>No-Code Overlays | Thanks!</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 24,
+        textAlign: 'center',
+        maxWidth: 600,
+        margin: '0 auto',
+        backgroundColor: '#000000dd',
+        padding: '48px 32px',
+        color: 'white',
+        borderRadius: '4px',
       }}
     >
       <img
@@ -13,14 +32,14 @@ const ThanksPage = () => (
         src="/nco-logo-round.png"
         width="100"
         height="100"
-        style={{ marginRight: 24 }}
+        style={{ width: 100, marginBottom: 12 }}
       />
       <h1>Thank you!</h1>
+      <p style={{ lineHeight: '1.3rem', marginTop: 32 }}>
+        Your application has been submitted. If you are selected as a tester,
+        you will receive an email within 7 business days.
+      </p>
     </div>
-    <p style={{ lineHeight: '1.3rem' }}>
-      Your application has been submitted. If you are selected as a tester, you
-      will receive an email within 7 business days.
-    </p>
   </div>
 );
 
